d6a7496fc848dff06ae85f215a9c6bd66910ec75
Tom Belcher <git@blchr.com>
2021-01-01 18:26:56 -0500

    Add config for webpack

---
+++ App/ClientApp/webpack.config.js
@@ -0,0 +1,37 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+    entry: "./src/index.tsx",
+    output: {
+        path: path.resolve(__dirname, "public"),
+        filename: "[name].[chunkhash].js",
+        publicPath: "/"
+    },
+    resolve: {
+        extensions: [".js", ".ts", ".tsx"]
+    },
+    module: {
+        rules: [
+            {
+                test: [ /\.ts$/, /\.tsx$/],
+                use: "ts-loader"
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: "./public/index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "css/[name].[chunkhash].css"
+        })
+    ]
+};
