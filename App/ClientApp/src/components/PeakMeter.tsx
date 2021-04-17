127694302740d1583366d86f528c44ebc234f37a
Tom <git@blchr.com>
2021-04-17 12:45:27 -0400

    Use a stream to handle volume messages rather than individual messages.
    Checking that component is mounted will eliminate memory leaks

--- App/ClientApp/src/components/PeakMeter.tsx
+++ App/ClientApp/src/components/PeakMeter.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as CSS from "csstype";
 import { SkipBack } from "react-feather";
-import { HubConnection } from "@microsoft/signalr";
+import { HubConnection, HubConnectionState } from "@microsoft/signalr";
 
 export interface IPeakMeterProps {
     vertical: boolean;
@@ -23,33 +23,61 @@ const PeakMeter = ({ vertical, connection, height, width }: IPeakMeterProps): Re
 
     var [state, setState] = React.useState<IPeakMetersState>(init);
     var volume = React.useRef<IPeakMetersState>(init);
+    var isMounted = React.useRef<boolean>(false);
 
     React.useEffect(() => {
+        isMounted.current = true;
         if (connection) {
             console.log("Audio connection established");
             connection.on("ReceiveVolume", volumeMessageHandler);
+            setTimeout(() => connection.stream("ReceiveVolumeChange")
+                    .subscribe({
+                        next: (message) => {
+                            //console.log("Volume message received");
+                            volumeMessageHandler(message);
+                        },
+                        complete: () => {
+                            console.log("Volume stream completed");
+                        },
+                        error: (err) => {
+                            console.error(err);
+                        }
+                    }), 5000);
         } else {
             console.log("No audio connection to ATEM");
         }
+
+        return () => {
+            // cleanup function
+            if (connection) {
+                connection.off("ReceiveVolume");
+            }
+            isMounted.current = false;
+        }
+
     }, [connection]);
 
     const volumeMessageHandler = (message: IPeakMetersState): void => {
-        try {
-            volume.current = message;
-            requestAnimationFrame(updateMeter);
-        }
-        catch (e) {
-            console.error(e);
+        if (isMounted.current) {
+            try {
+                volume.current = message;
+                requestAnimationFrame(updateMeter);
+            }
+            catch (e) {
+                console.error(e);
+            }
         }
     };
 
-    const updateMeter: FrameRequestCallback = (time: number) => {
-        setState(volume.current);
-        requestAnimationFrame(updateMeter);
+    const updateMeter: FrameRequestCallback = (time: number): void => {
+        if (isMounted.current) {
+            setState(volume.current);
+            requestAnimationFrame(updateMeter);
+        }
     };
 
     const dBFSToY = (db: number): number => {
-        //db = -2;
+        //db = -20;
 
         // db is between -Infinity and 0dB
         if (db < -60) return 300;
@@ -60,6 +88,7 @@ const PeakMeter = ({ vertical, connection, height, width }: IPeakMeterProps): Re
 
         // from 0 to -20dB is linear
         // lower than -20dB is logarithmic
+        // cutover is at 100px
         const y = db >= -60
             ? height * (Math.abs(db) / Math.abs(dBthreshold))
             : height * Math.log(Math.abs(db)) / Math.log(Math.abs(dBthreshold));        
@@ -69,9 +98,9 @@ const PeakMeter = ({ vertical, connection, height, width }: IPeakMeterProps): Re
 
     return <div className="audio-levels">
         {state.levels.map((c, index) => {
-            return <div>
-                <div key={index + "db"} className="audio-level-value">{c}</div>
-                <div key={index}
+            return <div key={index}>
+                <div key={index + "db"} className="audio-level-value">{state.peaks[index]}</div>
+                <div key={index + "level"}
                     className="audio-level"
                     style={{ clipPath: "inset(" + dBFSToY(c) + "px 2px 0 0)" }}></div>
             </div>
