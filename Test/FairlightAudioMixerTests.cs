d92288a352baea884ffac074615aa24ceda659ac
Tom <git@blchr.com>
2021-04-24 09:25:44 -0400

    Add tests for audio inputs and sources

--- Test/FairlightAudioMixerTests.cs
+++ Test/FairlightAudioMixerTests.cs
@@ -1,7 +1,9 @@
-﻿using NUnit.Framework;
+﻿using Microsoft.Extensions.Logging;
+using NUnit.Framework;
 using SwitcherServer.Atem;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace SwitcherServerTests
@@ -28,6 +30,29 @@ namespace SwitcherServerTests
             // assert
             Assert.IsNotNull(result);
             Assert.GreaterOrEqual(0, result.MasterOutFaderGain);
+        }
+
+        [Test]
+        public void CanGetFairlightAudioMixerInputs()
+        {
+            // act
+            var result = AtemMini.GetFairlightAudioMixer().Inputs;
+
+            // assert
+            Assert.IsNotNull(result);
+            Assert.GreaterOrEqual(result.Count(), 0);
+        }
+
+        [Test]
+        public void CanGetFairlightAudioMixerInputSources()
+        {
+            // act
+            var result = AtemMini.GetFairlightAudioMixer().Inputs.SelectMany(c => c.Sources);
+
+            // assert
+            Assert.IsNotNull(result);
+            Assert.GreaterOrEqual(result.Count(), 0);
+            result.ToList().ForEach(c => { Logger.LogInformation($"{c.Input.Id}/{c.Id}=> Active: {c.IsActive},{c.MixOption},{c.InputGain},{c.FaderGain}"); });
         }
     }
 }
