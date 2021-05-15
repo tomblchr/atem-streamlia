ea00db3371715672fa3e173c477e4ead9455e42a
Tom <git@blchr.com>
2021-05-15 18:43:48 -0400

    Make streaming optional.

--- App/Atem/StreamRTMP.cs
+++ App/Atem/StreamRTMP.cs
@@ -7,6 +7,12 @@ using System.Threading.Tasks;
 
 namespace SwitcherServer.Atem
 {
+    /// <summary>
+    /// Interact with streaming capabilities of the switcher
+    /// </summary>
+    /// <remarks>
+    /// Not all models support streaming
+    /// </remarks>
     public class StreamRTMP
     {
         private readonly IBMDSwitcherStreamRTMP _stream;
@@ -14,181 +20,38 @@ namespace SwitcherServer.Atem
 
         public StreamRTMP(IBMDSwitcherStreamRTMP stream, IMediator mediator)
         {
-            _stream = stream ?? new MockStream();
+            _stream = stream;
             _mediator = mediator;
 
-            _stream.AddCallback(new StreamRTMPCallback(mediator));
+            _stream?.AddCallback(new StreamRTMPCallback(mediator));
+        }
+
+        public bool IsStreamingSupported
+        {
+            get { return _stream != null;  }
         }
 
         public bool IsStreaming
         {
             get
             {
-                _stream.IsStreaming(out int streaming);
-                return streaming == 1;
+                if (IsStreamingSupported)
+                {
+                    _stream.IsStreaming(out int streaming);
+                    return streaming == 1;
+                }
+                return false;
             }
         }
 
         public void Start()
         {
-            _stream.StartStreaming();
+            _stream?.StartStreaming();
         }
 
         public void Stop()
         {
-            _stream.StopStreaming();
-        }
-    }
-
-    /// <summary>
-    /// The ATEM Mini does not support streaming, but I want to test this capability works
-    /// </summary>
-    class MockStream : IBMDSwitcherStreamRTMP
-    {
-        static _BMDSwitcherStreamRTMPState _state = _BMDSwitcherStreamRTMPState.bmdSwitcherStreamRTMPStateIdle;
-        static object _lock = new object();
-
-        public void StartStreaming()
-        {
-            lock (_lock)
-            {
-                _state = _BMDSwitcherStreamRTMPState.bmdSwitcherStreamRTMPStateStreaming;
-                _notifyStatus(_BMDSwitcherStreamRTMPState.bmdSwitcherStreamRTMPStateStreaming, _BMDSwitcherStreamRTMPError.bmdSwitcherStreamRTMPErrorNone);
-            }
-        }
-
-        public void StopStreaming()
-        {
-            lock (_lock)
-            {
-                _state = _BMDSwitcherStreamRTMPState.bmdSwitcherStreamRTMPStateIdle;
-                _notifyStatus(_BMDSwitcherStreamRTMPState.bmdSwitcherStreamRTMPStateIdle, _BMDSwitcherStreamRTMPError.bmdSwitcherStreamRTMPErrorNone);
-            }
-        }
-
-        public void IsStreaming(out int streaming)
-        {
-            if (_state == _BMDSwitcherStreamRTMPState.bmdSwitcherStreamRTMPStateStreaming)
-            {
-                streaming = 1;
-            }
-            else
-            {
-                streaming = 0;
-            }
-        }
-
-        public void GetStatus(out _BMDSwitcherStreamRTMPState state, out _BMDSwitcherStreamRTMPError error)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void SetServiceName(string serviceName)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void GetServiceName(out string serviceName)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void SetUrl(string url)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void GetUrl(out string url)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void SetKey(string url)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void GetKey(out string key)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void SetVideoBitrates(uint lowBitrate, uint highBitrate)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void GetVideoBitrates(out uint lowBitrate, out uint highBitrate)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void SetAudioBitrates(uint lowBitrate, uint highBitrate)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void GetAudioBitrates(out uint lowBitrate, out uint highBitrate)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void RequestDuration()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void GetDuration(out ulong duration)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void GetTimeCode(out byte hours, out byte minutes, out byte seconds, out byte frames, out int dropFrame)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void GetEncodingBitrate(out uint encodingBitrate)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void GetCacheUsed(out double cacheUsed)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void SetAuthentication(string username, string password)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void GetAuthentication(out string username, out string password)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void SetLowLatency(int lowLatency)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void GetLowLatency(out int lowLatency)
-        {
-            throw new NotImplementedException();
-        }
-
-        Action<_BMDSwitcherStreamRTMPEventType> _notify;
-        Action<_BMDSwitcherStreamRTMPState, _BMDSwitcherStreamRTMPError> _notifyStatus;
-        public void AddCallback(IBMDSwitcherStreamRTMPCallback callback)
-        {
-            _notify = callback.Notify;
-            _notifyStatus = callback.NotifyStatus;
-        }
-
-        public void RemoveCallback(IBMDSwitcherStreamRTMPCallback callback)
-        {
-            throw new NotImplementedException();
+            _stream?.StopStreaming();
         }
     }
 }
---
+++ Test/StreamRTMPTests.cs
@@ -0,0 +1,63 @@
+﻿using BMDSwitcherAPI;
+using MediatR;
+using NUnit.Framework;
+using SwitcherServer.Atem;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SwitcherServerTests
+{
+    [TestFixture]
+    public class StreamRTMPTests : Tests
+    {
+        IBMDSwitcherStreamRTMP _stream;
+        IMediator _mediator;
+
+        public StreamRTMPTests() : base()
+        {
+            _stream = AtemMini.SwitcherDirect.GetStreamRTMP() ?? new Mock.MockStream();
+            _mediator = new MockMediator();
+        }
+
+        [Test]
+        public void TestStreamingNotSupported()
+        {
+            // arrange
+            var s = new StreamRTMP(null, _mediator);
+
+            // act
+            s.Start();
+            s.Stop();
+
+            // assert
+            Assert.IsFalse(s.IsStreamingSupported);
+        }
+
+        [Test]
+        public void TestStartStream()
+        {
+            // arrange
+            var s = new StreamRTMP(_stream, _mediator);
+
+            // act
+            s.Start();
+
+            // assert
+            Assert.IsTrue(s.IsStreaming);
+        }
+
+        [Test]
+        public void TestStopStream()
+        {
+            // arrange
+            var s = new StreamRTMP(_stream, _mediator);
+
+            // act
+            s.Stop();
+
+            // assert
+            Assert.IsFalse(s.IsStreaming);
+        }
+    }
+}
