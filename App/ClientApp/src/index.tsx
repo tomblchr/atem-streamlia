b10cf40e90972043abb931c4ebb64b3d73140e97
Tom <git@blchr.com>
2021-11-18 21:24:45 -0500

    Remove the service worker as I really don't want it

--- App/ClientApp/src/index.tsx
+++ App/ClientApp/src/index.tsx
@@ -1,9 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.css';
-import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import unregister from './registerServiceWorker';
 
 const defaultUrl: string = ""; 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
@@ -15,5 +14,5 @@ ReactDOM.render(
   </BrowserRouter>,
   rootElement);
 
-registerServiceWorker();
+unregister();
 
