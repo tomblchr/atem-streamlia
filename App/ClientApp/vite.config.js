0e0f8ab3d02e2458f18c63ec8b46ec55067a96c6
Tom Belcher <git@blchr.com>
2024-02-03 10:41:14 -0500

    static-web-apps not able to handle git command

--- App/ClientApp/vite.config.js
+++ App/ClientApp/vite.config.js
@@ -3,7 +3,9 @@ import { defineConfig } from "vite";
 export default defineConfig((config) => {
 
     // concatenate current date and commit hash
-    const appversion = JSON.stringify(new Date().toISOString().split('T')[0] + '-' + require('child_process').execSync('git rev-parse --short HEAD').toString());
+    // githash currently failing https://github.com/Azure/static-web-apps/issues/1271
+    const githash = "unknown"; //require('child_process').execSync('git rev-parse --short HEAD').toString();
+    const appversion = JSON.stringify(new Date().toISOString().split('T')[0] + '-' + githash);
     
     console.log(`Build version: ${appversion}`);
     
